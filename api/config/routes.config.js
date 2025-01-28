a2b26cd47e4b35b43deba5f6273a0d54f03b838e
Carlos del Prado <carlos.delpradomota@telefonica.com>
2025-01-28 21:57:08 +0100

    feat: user model & register

--- api/config/routes.config.js
+++ api/config/routes.config.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const createError = require('http-errors');
 const events = require('../controllers/events.controller');
+const users = require('../controllers/users.controller');
 
 router.get('/events', events.list);
 router.post('/events', events.create);
@@ -10,12 +11,13 @@ router.get('/events/:id', events.detail);
 router.delete('/events/:id', events.delete);
 router.patch('/events/:id', events.update);
 
+router.post('/users', users.create);
+
 router.use((req, res, next) => {
   next(createError(404, 'Route not found'));
 });
 
 router.use((error, req, res, next) => {
-  console.error(error);
   if (error instanceof mongoose.Error.CastError && error.message.includes('_id')) error = createError(404, 'Resource not found');
   else if (error instanceof mongoose.Error.ValidationError) error = createError(400, error);
   else if (!error.status) error = createError(500, error.message);
@@ -26,7 +28,7 @@ router.use((error, req, res, next) => {
   if (error.errors) {
     data.errors = Object.keys(error.errors)
       .reduce((errors, errorKey) => {
-        errors[errorKey] = error.errors[errorKey].message;
+        errors[errorKey] = error.errors[errorKey]?.message || error.errors[errorKey];
         return errors;
       }, {});
   }
